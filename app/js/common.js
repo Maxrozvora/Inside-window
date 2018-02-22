7c02ba2afe2a4eac5316c52e784816a8d0ae369b
Maxrozvora <rozvora.max@gmail.com>
2018-02-22 15:00:54 +0200

    work on slider arrows

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		dots: true
+		dots: true,
+		nextArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+		prevArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>'
 	});
 	
   });
