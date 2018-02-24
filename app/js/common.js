17f0501dcf6d632fef0af6f588c9d958ef82cfa1
Maxrozvora <rozvora.max@gmail.com>
2018-02-24 22:40:29 +0200

    Map, footer section

--- app/js/common.js
+++ app/js/common.js
@@ -87,4 +87,49 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-prev"></button>',
 		prevArrow: '<button type="button" class="slick-next"></button>'
   });
+  ///////////////////////////////////////////////////
+
+  // Slider for popular section 
+  $('.room-slider__for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.room-slider__nav'
+  });
+  $('.room-slider__nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.room-slider__for', 
+    focusOnSelect: true,
+    nextArrow: '<button type="button" class="slick-prev"></button>',
+		prevArrow: '<button type="button" class="slick-next"></button>'
+  });
+
+  // Google maps
+
+  function initMap() {
+    var coordinates = {lat: 50.4516795, lng: 30.5208771}, // Координаты центра карты 
+        markerImg = 'img/marker.png', //  Иконка для маркера  
+   
+    // создаем карту и настраеваем 
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 16, // определяет первоначальный масштаб
+        disableDefaultUI: true, // убирает элементы управления
+        scrollwheel: false, // отключает масштабирование колесиком мыши (бывает полезно, если карта на всю ширину страницы и перебивает прокрутку вниз).
+    	
+    });
+
+    // маркер
+    marker = new google.maps.Marker({
+        position: coordinates, // координаты маркера 
+        map: map, //  ставим маркер в карту с id map
+        animation: google.maps.Animation.DROP, // анимация маркера DROP / BOUNCE
+        icon: markerImg,
+    });
+
+}
+// Запускаем карту при загрузки страницы
+google.maps.event.addDomListener(window, 'load', initMap); 
   });
