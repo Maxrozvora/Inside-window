41f7fc3328bf32a32303b1d7b31f9632579d1dc8
Maxrozvora <rozvora.max@gmail.com>
2018-02-24 17:57:32 +0200

    Catalog section end

--- app/js/common.js
+++ app/js/common.js
@@ -77,4 +77,14 @@ $(document).ready(function(){
     $(this).tab('show')
   })
   /////////////////////////////////////////////
+
+  // Catalog section slider
+
+  $('.catalog-slider').slick({
+    infinite: true,
+		slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-prev"></button>',
+		prevArrow: '<button type="button" class="slick-next"></button>'
+  });
   });
